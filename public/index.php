fe8588064ebe832b488b7d0be31319367c483d58
René de Kat <rdekat@etsy.com>
2025-08-28 17:47:17 +0100

    moves index.php to public directory for safety

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/src/bootstrap.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/bootstrap.php';
 
 use Service\GithubPRAudit;
 use Service\CsvExporter;
 
 // Load environment variables and repo JSON in bootstrap.php
 $token = $_ENV['GITHUB_TOKEN'];
-$repos = json_decode(file_get_contents(__DIR__ . '/config/repos_prs.json'), true);
+$repos = json_decode(file_get_contents(__DIR__ . '/../config/repos_prs.json'), true);
 
 $githubAudit = new GithubPRAudit($token, $repos);
 $auditData = $githubAudit->fetchAuditData();
